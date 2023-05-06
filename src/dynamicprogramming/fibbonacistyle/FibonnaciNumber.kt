12361ddd505bb50e14aa6505c583bd1fe5238c3f
baggio1103 <valijanov1103@mail.ru>
2023-05-07 01:19:32 +0300

    Add another approach for solving Fibonacci sequence using memoization

--- src/dynamicprogramming/fibbonacistyle/FibonnaciNumber.kt
+++ src/dynamicprogramming/fibbonacistyle/FibonnaciNumber.kt
@@ -1,5 +1,8 @@
 package dynamicprogramming.fibbonacistyle
 
+import java.lang.IllegalArgumentException
+import kotlin.system.measureTimeMillis
+
 class FibonacciNumber {
 
     fun fib(n: Int): Int {
@@ -8,14 +11,46 @@ class FibonacciNumber {
         return fib(n - 1) + fib(n - 2)
     }
 
+    fun fibMemoized(n: Int, map: MutableMap<Int, Int> = mutableMapOf(0 to 0, 1 to 1)): Int {
+        if (map.containsKey(n)) {
+            return map[n] ?: throw IllegalArgumentException("Value cannot be null")
+        }
+        map[n] = fibMemoized(n - 1, map) + fibMemoized(n - 2, map)
+        return map[n] ?: throw IllegalArgumentException("Value cannot be null")
+    }
 }
 
 fun main() {
     val fibonacciNumber = FibonacciNumber()
-    println(
-        fibonacciNumber.fib(10)
-    )
-    println(
-        fibonacciNumber.fib(30)
-    )
+    measureTimeMillis {
+        println(
+            fibonacciNumber.fib(15)
+        )
+        println(
+            fibonacciNumber.fib(25)
+        )
+        println(
+            fibonacciNumber.fib(35)
+        )
+        println(
+            fibonacciNumber.fib(45)
+        )
+    }.let { println("Exec time of fibonacciSequence: $it") }
+    println("=-=-=-=-=-=-=-=-=-=-=-=")
+    measureTimeMillis {
+        println(
+            fibonacciNumber.fibMemoized(15)
+        )
+        println(
+            fibonacciNumber.fibMemoized(25)
+        )
+        println(
+            fibonacciNumber.fibMemoized(35)
+        )
+        println(
+            fibonacciNumber.fibMemoized(45)
+        )
+    }.let {
+        println("Exec time of fibonacciSequence with memoization: $it")
+    }
 }
