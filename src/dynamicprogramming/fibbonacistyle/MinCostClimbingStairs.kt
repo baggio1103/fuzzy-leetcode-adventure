d95fbb8be6d13e2c4cb032b9f36d7bc93b4cc94d
baggio1103 <valijanov1103@mail.ru>
2023-05-07 18:18:31 +0300

    Solve next two problems from the Leetcode Dynamic Programming section

---
+++ src/dynamicprogramming/fibbonacistyle/MinCostClimbingStairs.kt
@@ -0,0 +1,37 @@
+package dynamicprogramming.fibbonacistyle
+
+import kotlin.math.min
+
+class MinCostClimbingStairs {
+
+    fun minCostClimbingStairs(stairCosts: IntArray,
+                              index: Int = -1,
+                              map: MutableMap<Int, Int> = mutableMapOf()
+    ): Int {
+        if (map.containsKey(index)) {
+            return map[index] ?: throw IllegalArgumentException("Value cannot be null")
+        }
+        if (index >= stairCosts.size) {
+            return 0
+        }
+        val left = minCostClimbingStairs(stairCosts, index + 1, map)
+        val right = minCostClimbingStairs(stairCosts, index + 2, map)
+        val value = min(left, right) + if (index != -1) stairCosts[index] else 0
+        map[index] = value
+        return value
+    }
+
+}
+
+fun main() {
+    val minCostClimbingStairs = MinCostClimbingStairs()
+    println(
+        minCostClimbingStairs.minCostClimbingStairs(intArrayOf(10, 15, 1, 2, 25, 30))
+    )
+    println(
+        minCostClimbingStairs.minCostClimbingStairs(intArrayOf(10, 15, 20), -1)
+    )
+    println(
+        minCostClimbingStairs.minCostClimbingStairs(intArrayOf(1, 100, 1, 1, 1, 100, 1, 1, 100, 1), -1)
+    )
+}
---
+++ src/dynamicprogramming/fibbonacistyle/TribonacciNumber.kt
@@ -0,0 +1,39 @@
+package dynamicprogramming.fibbonacistyle
+
+import kotlin.IllegalArgumentException
+import kotlin.system.measureTimeMillis
+
+class TribonacciNumber {
+
+    fun tribonacci(n: Int): Int {
+        if (n == 0) {
+            return 0
+        }
+        if (n == 1 || n == 2) {
+            return 1
+        }
+        return tribonacci(n - 3) + tribonacci(n - 2) + tribonacci(n - 1)
+    }
+
+    fun tribonnacciMemoized(n: Int, map: MutableMap<Int, Int> = mutableMapOf(0 to 0, 1 to 1, 2 to 1)): Int {
+        if (map.containsKey(n)) {
+            return map[n] ?: throw IllegalArgumentException("Value cannot be null")
+        }
+        map[n] = tribonnacciMemoized(n - 3, map) + tribonnacciMemoized(n - 2, map) + tribonnacciMemoized(n - 1, map)
+        return map[n] ?: throw IllegalArgumentException("Value cannot be null")
+    }
+
+}
+
+fun main() {
+    val tribonacciNumber = TribonacciNumber()
+    println(
+        tribonacciNumber.tribonacci(4)
+    )
+    measureTimeMillis {
+        tribonacciNumber.tribonacci(36)
+    }.let { println("Exec time for tribonacci(36): $it ms") }
+    measureTimeMillis {
+        tribonacciNumber.tribonnacciMemoized(36)
+    }.let { println("Exec time for tribonnacciMemoized(36): $it ms") }
+}
