59d6da0ca9b775092a29b34544e57c5b44734594
baggio1103 <valijanov1103@mail.ru>
2023-05-18 23:54:04 +0300

    Solve daily challenge for May15 correctly

--- src/dailyChallenge/may15/SwappingNodes.kt
+++ src/dailyChallenge/may15/SwappingNodes.kt
@@ -1,56 +1,153 @@
 package dailyChallenge.may15
 
-import kotlin.math.absoluteValue
+import kotlin.math.max
 import kotlin.math.min
 
-class ListNode(var value: Int) {
+class ListNode(var `val`: Int) {
      var next: ListNode? = null
  }
 
 class SwappingNodes {
 
+
     fun swapNodes(head: ListNode?, k: Int): ListNode? {
-        if (head?.next == null) {
+        val nodeMap = nodes(head)
+        val size = nodeMap.size
+        val start = nodeMap[min(k, size - k + 1)]
+        val finish = nodeMap[max(k, size - k + 1)]
+        println("-=-=-=-=-=-=-=-=-=")
+        nodeMap.forEach { (_, value) ->
+            print("${value.`val`} ")
+        }
+        println("/ K = $k \n-=-=-=-=-=-=-=-=-=")
+        if (start == finish) {
             return head
         }
-        val (starting, ending, nodes) = nodes(head, k)
-        var currentNode = starting
-        for (i in nodes.lastIndex downTo 0) {
-            currentNode.next = nodes[i]
-            currentNode = currentNode.next ?: throw IllegalArgumentException("Value cannot be null")
+        if (size == 2) {
+            start?.next = null
+            finish?.next = start
+            return finish
         }
-        currentNode.next = ending
-        return head
+        val (startPrevIndex, startNextIndex) = Pair(min(size - k, k -1), min(k + 1, size - k + 2))
+
+        val (finishPrevIndex, finishNextIndex) = Pair(
+            max(size - k, k - 1),
+            max(k + 1, size - k + 2)
+        )
+        val isNeighbour = k == size - k
+        val (startPrev, startNext) = Pair(nodeMap[startPrevIndex], nodeMap[startNextIndex])
+        val (finishPrev, finishNext) = Pair(nodeMap[finishPrevIndex], nodeMap[finishNextIndex])
+        startPrev?.let {
+            it.next = finish
+        }
+        finish?.let {
+            if (isNeighbour) {
+                it.next = start
+            } else {
+                it.next = startNext
+            }
+        }
+        finishPrev?.let {
+            if (isNeighbour) {
+                it.next = finishNext
+            } else {
+                it.next = start
+            }
+        }
+        start?.let {
+            it.next = finishNext
+        }
+        return if (k == 1 || k == size) finish else head
     }
 
-    private fun nodes(head: ListNode?, k: Int): Triple<ListNode, ListNode, List<ListNode>> {
-        val nodes = mutableListOf<ListNode>()
+    fun nodes(head: ListNode?): Map<Int, ListNode> {
         var node = head
+        val nodeMap = mutableMapOf<Int, ListNode>()
+        var count = 1
         while (node != null) {
-            nodes.add(node)
+            nodeMap[count++] = node
             node = node.next
         }
-        return if (nodes.size > 1) {
-            Triple(
-                nodes[k.minus(2).absoluteValue],
-                nodes[min(nodes.size - k + 1, nodes.lastIndex)],
-                nodes.let { it.subList(k - 1, it.size - k + 1) }
-            )
-
-        } else {
-            Triple(
-                nodes[0],
-                nodes[0],
-                nodes
-            )
-        }
+        return nodeMap
     }
 
 }
 
 fun main() {
     val swappingNodes = SwappingNodes()
-    val head = ListNode(1).also {
+
+    printInfo(
+        swappingNodes.swapNodes(ListNode(10).also {
+            it.next = ListNode(25).also {
+                it.next = ListNode(35)
+            }
+        }, 2)
+    )
+    printInfo(
+        swappingNodes.swapNodes(
+            ListNode(7).also {
+
+                it.next = ListNode(9)
+                    .also {
+                        it.next = ListNode(6).also {
+                            it.next = ListNode(6).also {
+                                it.next = ListNode(7).also {
+                                    it.next = ListNode(8).also {
+                                        it.next = ListNode(3)
+                                            .also {
+                                                it.next = ListNode(0).also {
+                                                    it.next = ListNode(9).also {
+                                                        it.next = ListNode(5)
+                                                    }
+                                                }
+                                            }
+                                    }
+                                }
+                            }
+                        }
+                    }
+            }, 5
+        )
+    )
+
+    printInfo(
+        swappingNodes.swapNodes(ListNode(1).also {
+            it.next = ListNode(2).also {
+                it.next = ListNode(3).also {
+                    it.next = ListNode(4).also {
+                        it.next = ListNode(5)
+                    }
+                }
+            }
+        }, 1)
+    )
+    println("-=-=-=-=-=-=-=-=-=")
+    printInfo(
+        swappingNodes.swapNodes(
+            ListNode(1).also {
+                it.next = ListNode(2).also {
+                    it.next = ListNode(3).also {
+                        it.next = ListNode(4).also {
+                            it.next = ListNode(5)
+                        }
+                    }
+                }
+            }, 5
+        )
+    )
+
+    println("-=-=-=-=-=-=-=-=-=")
+    printInfo(swappingNodes.swapNodes(ListNode(10), 1))
+
+    println("-=-=-=-=-=-=-=-=-=")
+    printInfo(
+        swappingNodes.swapNodes(ListNode(10).also {
+            it.next = ListNode(25)
+        }, 1)
+    )
+
+    println("-=-=-=-=-=-=-=-=-=")
+    printInfo(swappingNodes.swapNodes(ListNode(1).also {
         it.next = ListNode(2).also {
             it.next = ListNode(3).also {
                 it.next = ListNode(4).also {
@@ -58,25 +155,39 @@ fun main() {
                 }
             }
         }
-    }
-    val node = swappingNodes.swapNodes(head, 2)
-    printInfo(node)
+    }, 2))
+
+    println("-=-=-=-=-=-=-=-=-=")
+    printInfo(swappingNodes.swapNodes(ListNode(1).also {
+        it.next = ListNode(2).also {
+            it.next = ListNode(3).also {
+                it.next = ListNode(4).also {
+                    it.next = ListNode(5)
+                }
+            }
+        }
+    }, 3))
+//
 //    printInfo(
 //        swappingNodes.swapNodes(ListNode(1), 1)
 //    )
-    printInfo(
-        swappingNodes.swapNodes(ListNode(1).also {
-            it.next = ListNode(2).also {
-                it.next = ListNode(3)
-            }
-        }, 1)
-    )
+//
+//    println("-=-=-=-=-=-=-=-=-=")
+//
+//    printInfo(
+//        swappingNodes.swapNodes(ListNode(1).also {
+//            it.next = ListNode(2).also {
+//                it.next = ListNode(3)
+//            }
+//        }, 1)
+//    )
 }
 
 fun printInfo(node: ListNode?) {
     var currentNode = node
     while (currentNode != null) {
-        println(currentNode.value)
+        println(currentNode.`val`)
         currentNode = currentNode.next
     }
 }
+
