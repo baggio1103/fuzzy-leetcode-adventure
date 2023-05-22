5810d0a997f36b07fb984ac2fe49f7720efda77e
baggio1103 <valijanov1103@mail.ru>
2023-05-22 12:27:30 +0300

    Solve daily challenge for May 22

---
+++ src/dailyChallenge/may22/TopKFreequent.kt
@@ -0,0 +1,38 @@
+package dailyChallenge.may22
+
+class TopKFrequentElement {
+
+    fun topKFrequent(numbers: IntArray, k: Int): IntArray {
+        val mutableMap = mutableMapOf<Int, Int>()
+        numbers.forEach {
+            val value = mutableMap[it]
+            value?.let { key ->
+                mutableMap[it] = key + 1
+            } ?: kotlin.run {
+                mutableMap[it] = 1
+            }
+        }
+        return mutableMap.toList().sortedByDescending { it.second }.map { it.first }.take(k).toIntArray()
+    }
+
+}
+
+fun IntArray.info() {
+    println("-=-=-=-=-=-=-=")
+    this.forEach {
+        print("$it ")
+    }
+    println("\n-=-=-=-=-=-=-=")
+}
+
+fun main() {
+    val topKFrequentElement = TopKFrequentElement()
+
+    topKFrequentElement.topKFrequent(intArrayOf(5, 5, 5, 3, 2, 5, 5, 2, 2, 3), 2).info()
+
+    topKFrequentElement.topKFrequent(intArrayOf(1), 1).info()
+
+    topKFrequentElement.topKFrequent(intArrayOf(-1, -1), 1).info()
+
+}
+
