93255ef3f32f0ca9b717cc56b115ac8e7808cfa7
baggio1103 <valijanov1103@mail.ru>
2023-05-31 23:23:23 +0300

    Solve daily challenge for May 30 - Design primitive HASHSET implementation

---
+++ src/dailyChallenge/may30/MyHashSet.kt
@@ -0,0 +1,38 @@
+package dailyChallenge.may30
+
+class MyHashSet {
+
+    private val array = MutableList(1000000 + 1) {
+        _ -> -1
+    }
+
+    fun add(key: Int) {
+        array[key] = key
+    }
+
+    fun remove(key: Int) {
+        array[key] = -1
+    }
+
+    fun contains(key: Int): Boolean {
+        return array[key] != -1
+    }
+
+}
+
+fun main() {
+    val myHashSet = MyHashSet()
+    myHashSet.remove(0)
+    myHashSet.add(1)
+    myHashSet.add(2)
+    myHashSet.add(3)
+    myHashSet.contains(3)
+    myHashSet.add(22)
+    myHashSet.add(2)
+    myHashSet.add(2)
+    myHashSet.add(1000000)
+    myHashSet.remove(2)
+    println(
+        myHashSet.contains(2)
+    )
+}
