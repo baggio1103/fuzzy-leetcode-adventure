90f081e7eca3048585a628b99115412f277c7d35
baggio1103 <valijanov1103@mail.ru>
2023-06-11 23:58:18 +0300

    Add daily challenge for June 11

---
+++ src/dailyChallenge/june/june11/SnapshotArray.kt
@@ -0,0 +1,22 @@
+package dailyChallenge.june.june11
+
+class SnapshotArray(length: Int) {
+
+    fun set(index: Int, `val`: Int) {
+
+    }
+
+    fun snap(): Int {
+        return 1
+    }
+
+    fun get(index: Int, snapId: Int): Int {
+        return 1
+
+    }
+
+}
+
+fun main() {
+
+}
