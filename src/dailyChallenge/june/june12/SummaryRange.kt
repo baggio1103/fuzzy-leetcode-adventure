24b98b17d881d6b4295190a3d78dd790149bbf9f
baggio1103 <valijanov1103@mail.ru>
2023-06-12 11:46:59 +0300

    Solve daily challenge for June 12

---
+++ src/dailyChallenge/june/june12/SummaryRange.kt
@@ -0,0 +1,44 @@
+package dailyChallenge.june.june12
+
+class SummaryRange {
+
+    fun summaryRanges(array: IntArray): List<String> {
+        if (array.isEmpty()) {
+            return emptyList()
+        }
+        val ranges = mutableListOf<String>()
+        var startingIndex: Int? = null
+        for (i in 0 until array.lastIndex) {
+            if (array[i + 1] - array[i] == 1) {
+                if (startingIndex == null) {
+                    startingIndex = i
+                }
+            } else {
+                startingIndex?.let {
+                    ranges.add("${array[it]}->${array[i]}")
+                } ?: run {
+                    ranges.add("${array[i]}")
+                }
+                startingIndex = null
+            }
+        }
+        startingIndex?.let {
+            ranges.add("${array[startingIndex]}->${array[array.lastIndex]}")
+        } ?: run {
+            ranges.add("${array[array.lastIndex]}")
+        }
+        return ranges
+    }
+
+}
+
+fun main() {
+    val summaryRange = SummaryRange()
+    println(summaryRange.summaryRanges(
+        intArrayOf(0,1,2,4,5,7)
+    ))
+    println(summaryRange.summaryRanges(
+        intArrayOf(0,2,3,4,6,8,9)
+    ))
+
+}
