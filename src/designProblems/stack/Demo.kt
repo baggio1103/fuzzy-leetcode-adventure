f95183249c5ca00fd216e2686a7c25552072a6a2
baggio1103 <valijanov1103@mail.ru>
2023-06-16 00:30:48 +0300

    Add more tests on Max Stack

--- src/designProblems/stack/Demo.kt
+++ src/designProblems/stack/Demo.kt
@@ -1,32 +1,42 @@
 package designProblems.stack
 
 fun main() {
+    println("-=-=-=-=-=-=-=-=")
+    exampleOne()
+    println("-=-=-=-=-=-=-=-=")
+    exampleTwo()
+}
+
+fun exampleOne() {
     val maxStack = MaxStack<Int>()
     maxStack.push(8)
     maxStack.push(2)
     maxStack.push(4)
     maxStack.push(6)
-    println(
-        maxStack.max()
-    )
+    println(maxStack.max()) // 8
     maxStack.push(9)
     maxStack.push(3)
-    println(
-        "Peek: ${        maxStack.peek()
-        }"
-    )
-    println(
-        maxStack.max()
-    )
-    println(
-        "Size: ${maxStack.size} Pop must be 3 - ${
-            maxStack.pop()
-        } Size: ${maxStack.size}"
-    )
-    println(
-        "Shit: ${        maxStack.pop()
-        }"
-    )
+    println(maxStack.max()) // 9
     maxStack.pop()
-    println(maxStack.max())
+    maxStack.pop()
+    println(maxStack.max()) // 8
+    maxStack.pop()
+    maxStack.pop()
+    maxStack.pop()
+    println(maxStack.max()) // 8
+    maxStack.pop()
+    println(maxStack.size) // Size = 0
+}
+
+fun exampleTwo() {
+    val stack = MaxStack<Int>()
+    repeat(5) {
+        stack.push(it * 3)
+    }
+    println(stack.size) // 5
+    println(stack.max()) // 12
+    stack.pop()
+    println(stack.max()) // 9
+    stack.pop()
+    println(stack.max()) // 6
 }
