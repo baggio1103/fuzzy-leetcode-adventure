9c402cd6dd88a694794e73d57d779040f82798a2
baggio1103 <valijanov1103@mail.ru>
2023-06-19 21:18:29 +0300

    Solve the challenge fully and add examples

--- src/leetcodefavorites/robot/Demo.kt
+++ src/leetcodefavorites/robot/Demo.kt
@@ -3,8 +3,18 @@ package leetcodefavorites.robot
 import java.io.File
 
 fun main() {
+    inputFromFile()
+}
+
+fun inputFromFile() {
     val robot = Robot()
     val input = File("input.txt").readLines().first()
     val coordinate = robot.defineCoordinate(input.toInt())
     File("output.txt").writeText("${coordinate.x} ${coordinate.y}")
+}
+
+fun stdInput() {
+    val robot = Robot()
+    val coordinate = robot.defineCoordinate(10)
+    println(coordinate)
 }
