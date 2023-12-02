98d242097660a7f993994fd49a1de00a17bfda09
baggio1103 <valijanov1103@mail.ru>
2023-12-02 16:36:33 +0300

    Solve daily challenge for December 2

---
+++ src/dailyChallenge/december/december2/CountCharacters.kt
@@ -0,0 +1,27 @@
+package dailyChallenge.december.december2
+
+
+// words = ["cat","bt","hat","tree"], chars = "atach"
+
+fun countCharacters(words: Array<String>, chars: String): Int {
+    val charMap = chars.groupingBy { it }.eachCount()
+    return words.toList().parallelStream().filter { word ->
+        word.groupingBy { it }.eachCount()
+            .forEach {
+            val (key, value) = it
+            if (charMap[key] == null || value > (charMap[key] ?: 0)) {
+                return@filter false
+            }
+        }
+        return@filter true
+    }.toList().sumOf { it.length }
+}
+
+fun main() {
+    println(
+        countCharacters(words = arrayOf("cat", "bt", "hat", "tree"), chars = "atach")
+    )
+    println(
+        countCharacters(words = arrayOf("hello", "world", "leetcode"), "welldonehoneyr")
+    )
+}
