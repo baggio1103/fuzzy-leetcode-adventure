2608fca9805c5291fd684ca8ba46c197ef93f20c
baggio1103 <valijanov1103@mail.ru>
2023-12-23 02:16:20 +0300

    Solve daily challenge for December 22

--- src/dailyChallenge/december/december22/MaxScore.kt
+++ src/dailyChallenge/december/december22/MaxScore.kt
@@ -1,9 +1,34 @@
 package dailyChallenge.december.december22
 
 fun maxScore(string: String): Int {
-    return 0
+    var max = 0
+    val ones = string.count { it == '1' }
+    var currentZeros = 0
+    var currentOnes = 0
+    for (i in 0 until string.lastIndex) {
+        val char = string[i]
+        val sum = if (char == '0') {
+            currentZeros++
+            currentZeros + (ones - currentOnes)
+        } else {
+            currentOnes++
+            currentZeros + (ones - currentOnes)
+        }
+        if (sum > max) {
+            max = sum
+        }
+    }
+    return max
 }
 
 fun main() {
-
+    println(
+        maxScore("011101")
+    )
+    println(
+        maxScore("00111")
+    )
+    println(
+        maxScore("1111")
+    )
 }
