e005995f922eb40bf86f5c838e33daf2392e225e
baggio1103 <valijanov1103@mail.ru>
2024-01-18 11:38:40 +0500

    Add another approach to solve challenge

--- src/dailyChallenge/year2024/january/january18/ClimbStairs.kt
+++ src/dailyChallenge/year2024/january/january18/ClimbStairs.kt
@@ -17,11 +17,31 @@ fun climbStairs(n: Int, stair: Int = 0, cache: MutableMap<Int, Int> = mutableMap
     return stepOne + stepTwo
 }
 
+fun climbStairs(n: Int, cache: MutableMap<Int, Int> = mutableMapOf()): Int {
+    val cached = cache[n]
+    if (cached != null) {
+        return cached
+    }
+    if (n == 0) {
+        return 1
+    }
+    if (n < 0) {
+        return 0
+    }
+    val oneStep = climbStairs(n - 1, cache)
+    val twoStep = climbStairs(n - 2, cache)
+    cache[n] = oneStep + twoStep
+    return oneStep + twoStep
+}
+
 fun main() {
     println(
-        climbStairs(45)
+        climbStairs(3)
     )
     println(
-        climbStairs(3)
+        climbStairs(4)
+    )
+    println(
+        climbStairs(45)
     )
 }
