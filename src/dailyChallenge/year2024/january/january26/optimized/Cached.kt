6db663634c78ab8c341276723aec1ddc3a114808
baggio1103 <valijanov1103@mail.ru>
2024-01-27 00:09:42 +0500

    Solve using memoization and refactor

--- src/dailyChallenge/year2024/january/january26/optimized/Cached.kt
+++ src/dailyChallenge/year2024/january/january26/optimized/Cached.kt
@@ -1,7 +1,10 @@
 package dailyChallenge.year2024.january.january26.optimized
 
-private val mod = 1_000_000_007
-fun findPaths(m: Int, n: Int, maxMove: Int, startRow: Int, startColumn: Int, cache: MutableMap<String, Int> = mutableMapOf()): Int {
+private const val mod = 1_000_000_007
+
+fun findPaths(
+    m: Int, n: Int, maxMove: Int, startRow: Int, startColumn: Int, cache: MutableMap<String, Int> = mutableMapOf()
+): Int {
     val key = "$maxMove-$startRow-$startColumn"
     val cached = cache[key]
     if (cached != null) {
@@ -23,13 +26,7 @@ fun findPaths(m: Int, n: Int, maxMove: Int, startRow: Int, startColumn: Int, cac
     val down = findPaths(m, n, maxMove - 1, startRow + 1, startColumn, cache)
     val left = findPaths(m, n, maxMove - 1, startRow, startColumn - 1, cache)
     val right = findPaths(m, n, maxMove - 1, startRow, startColumn + 1, cache)
-    var result = up
-    result += down
-    result %= mod
-    result += left
-    result %= mod
-    result += right
-    result %= mod
+    val result = (((((up + down) % mod) + left) % mod) + right) % mod
     cache[key] = result
     return result
 }
@@ -41,13 +38,13 @@ fun main() {
         )
     )
     println(
-        findPaths(1, 3, 3, 0, 1)
+        findPaths(
+            1, 3, 3, 0, 1
+        )
     )
-    val value = findPaths(
-        8, 50, 23, 5, 26
-    )
-
     println(
-        value % mod
+        findPaths(
+            8, 50, 23, 5, 26
+        )
     )
 }
