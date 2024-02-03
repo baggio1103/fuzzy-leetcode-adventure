fe3cdeb0f1dd225d70c953203308ce797b309dac
baggio1103 <valijanov1103@mail.ru>
2024-02-03 23:52:12 +0500

    Add daily challenge for February 3

---
+++ src/dailyChallenge/year2024/february/february3/PartitionArray.kt
@@ -0,0 +1,22 @@
+package dailyChallenge.year2024.february.february3
+
+fun maxSumAfterPartitioning(arr: IntArray, k: Int): Int {
+    return 1
+
+}
+
+fun main() {
+    println(
+        maxSumAfterPartitioning(
+            intArrayOf(1, 15, 7, 9, 2, 5, 10), 3
+        )
+    )
+    println(
+        maxSumAfterPartitioning(
+            intArrayOf(1, 4, 1, 5, 7, 3, 6, 1, 9, 9, 3), 3
+        )
+    )
+    println(
+        intArrayOf(5, 7, 7, 7, 7, 9, 9, 9, 9, 9).sum()
+    )
+}
