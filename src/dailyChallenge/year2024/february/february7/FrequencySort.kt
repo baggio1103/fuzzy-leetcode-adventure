e3189e9502db1962a72158b2f25e1287fe3abbfc
baggio1103 <Valijanov1103@mail.ru>
2024-02-07 03:25:58 +0300

    Solve daily challenge for February 7

---
+++ src/dailyChallenge/year2024/february/february7/FrequencySort.kt
@@ -0,0 +1,31 @@
+package dailyChallenge.year2024.february.february7
+
+import java.util.PriorityQueue
+
+fun frequencySort(string: String): String {
+    val queue = PriorityQueue(Comparator.comparing<Pair<Int, Char>, Int> { it.first }.reversed()).apply {
+        string.groupingBy { it }.eachCount().forEach { (char, count) ->
+            add(Pair(count, char))
+        }
+    }
+    return buildString {
+        while (queue.isNotEmpty()) {
+            val (count, char) = queue.poll()
+            repeat(count) {
+                append(char)
+            }
+        }
+    }
+}
+
+fun main() {
+    println(
+        frequencySort("tree")
+    )
+    println(
+        frequencySort("cccaaa")
+    )
+    println(
+        frequencySort("Aabb")
+    )
+}
