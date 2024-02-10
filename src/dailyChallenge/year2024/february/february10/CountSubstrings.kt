c7cfc3035442eba4d5efc524617ce9f5e5516b56
baggio1103 <Valijanov1103@mail.ru>
2024-02-10 08:31:10 +0300

    Solve daily challenge for February 10

---
+++ src/dailyChallenge/year2024/february/february10/CountSubstrings.kt
@@ -0,0 +1,46 @@
+package dailyChallenge.year2024.february.february10
+
+fun countSubstrings(string: String): Int {
+    var counter = 0
+    for (i in string.indices) {
+        val size = i + 1
+        val charArray = CharArray(size) { index -> string[index] }
+        for (j in 0 .. string.lastIndex - i) {
+            val substring = string.substring(j..j + i)
+            if (isPalindrome(substring)) {
+                counter++
+            }
+        }
+    }
+    return counter
+}
+
+fun isPalindrome(substring: String): Boolean {
+    var i = 0
+    var j = substring.lastIndex
+    while (j > i) {
+        if (substring[i] != substring[j]) {
+            return false
+        }
+        i++
+        j--
+    }
+    return true
+}
+
+fun main() {
+//    println(
+//        isPalindrome("aba")
+//    )
+//    println(
+//        isPalindrome("abc")
+//    )
+//    println(
+//        isPalindrome("abba")
+//    )
+    println(
+        countSubstrings(
+            "aaa"
+        )
+    )
+}
