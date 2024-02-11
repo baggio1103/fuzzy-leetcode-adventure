a143a5b6e7aa919058f4ad365b72e524d628f67e
baggio1103 <valijanov1103@mail.ru>
2024-02-11 22:47:52 +0500

    Add daily challenge for February 11

---
+++ src/dailyChallenge/year2024/february/february11/CherryPickup.kt
@@ -0,0 +1,18 @@
+package dailyChallenge.year2024.february.february11
+
+fun cherryPickup(grid: Array<IntArray>): Int {
+    return 0
+}
+
+fun main() {
+    println(
+        cherryPickup(
+            arrayOf(
+                intArrayOf(3, 1, 1),
+                intArrayOf(2, 5, 1),
+                intArrayOf(1, 5, 5),
+                intArrayOf(2, 1, 1),
+            )
+        )
+    )
+}
