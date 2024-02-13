c51ae871e4e6db9b3caba8187c5dd0ff02fe685c
baggio1103 <valijanov1103@mail.ru>
2024-02-13 12:05:51 +0500

    Refactor solution

--- src/dailyChallenge/year2024/february/february13/FirstPalindrome.kt
+++ src/dailyChallenge/year2024/february/february13/FirstPalindrome.kt
@@ -1,14 +1,16 @@
 package dailyChallenge.year2024.february.february13
 
 fun firstPalindrome(words: Array<String>): String {
-    return words.firstOrNull { isPalindrome(it) } ?: ""
+    for (word in words) {
+        if (isPalindrome(word)) return word
+    }
+    return ""
 }
 
 fun isPalindrome(string: String, start: Int = 0, end: Int = string.lastIndex): Boolean {
-    if (start > end) {
-        return true
-    }
-    return string[start] == string[end] && isPalindrome(string, start + 1, end - 1)
+    if (start > end) return true
+    if (string[start] != string[end]) return false
+    return isPalindrome(string, start + 1, end - 1)
 }
 
 fun main() {
