1c9d512a176fa98a08ebf779bd7e680ab2f41b57
baggio1103 <valijanov1103@mail.ru>
2024-02-18 23:54:26 +0500

    Add daily challenge for February 18

---
+++ src/dailyChallenge/year2024/february/february18/MeetingRooms-III.kt
@@ -0,0 +1,20 @@
+package dailyChallenge.year2024.february.february18
+
+fun mostBooked(n: Int, meetings: Array<IntArray>): Int {
+    return 0
+}
+
+fun main() {
+    println(
+        mostBooked(
+            3,
+            arrayOf(
+                intArrayOf(1, 20),
+                intArrayOf(2, 10),
+                intArrayOf(3, 5),
+                intArrayOf(4, 9),
+                intArrayOf(6, 8)
+            )
+        )
+    )
+}
