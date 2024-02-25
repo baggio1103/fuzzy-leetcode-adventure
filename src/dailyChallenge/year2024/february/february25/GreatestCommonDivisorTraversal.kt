fcbbdae8a534d81fdcdd49b5fe68a530213fcd4a
baggio1103 <valijanov1103@mail.ru>
2024-02-25 23:50:06 +0500

    Add daily challenge for February 25

---
+++ src/dailyChallenge/year2024/february/february25/GreatestCommonDivisorTraversal.kt
@@ -0,0 +1,13 @@
+package dailyChallenge.year2024.february.february25
+
+fun canTraverseAllPairs(nums: IntArray): Boolean {
+    return true
+}
+
+fun main() {
+    println(
+        canTraverseAllPairs(
+            intArrayOf(2, 3, 6)
+        )
+    )
+}
