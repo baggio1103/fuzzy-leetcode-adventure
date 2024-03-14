9836930ee37a8671b7372ef9cca18100462e7d0a
baggio1103 <valijanov1103@mail.ru>
2024-03-14 23:02:11 +0400

    Solve daily challenge for March 14

---
+++ src/dailyChallenge/year2024/march/march13/BinarySubArrays.kt
@@ -0,0 +1,28 @@
+package dailyChallenge.year2024.march.march13
+
+fun numSubarraysWithSum(nums: IntArray, goal: Int): Int {
+    var count = 0
+    for (i in nums.indices) {
+        for (j in 0 until nums.size - i) {
+            var sum = 0
+            for (y in j..j + i) {
+                if (nums[y] == 1) sum++
+            }
+            if (sum == goal) count++
+        }
+    }
+    return count
+}
+
+fun main() {
+    println(
+        numSubarraysWithSum(
+            intArrayOf(1, 0, 1, 0, 1), 2
+        )
+    )
+//    println(
+//        numSubarraysWithSum(
+//            intArrayOf(0, 0, 0, 0, 0), 0
+//        )
+//    )
+}
