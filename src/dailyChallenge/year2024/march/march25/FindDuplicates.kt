46a28c435af2a44036c0e37694a1fcf6de9662f5
baggio1103 <valijanov1103@mail.ru>
2024-03-25 14:38:56 +0400

    Solve daily challenge for March 25

---
+++ src/dailyChallenge/year2024/march/march25/FindDuplicates.kt
@@ -0,0 +1,28 @@
+package dailyChallenge.year2024.march.march25
+
+import kotlin.math.absoluteValue
+
+fun findDuplicates(array: IntArray): List<Int> {
+    val list = mutableListOf<Int>()
+    for (i in array.indices) {
+        val index = array[i].absoluteValue - 1
+        array[index] = -array[index]
+        if (array[index] > 0) {
+            list.add(array[i].absoluteValue)
+        }
+    }
+    return list
+}
+
+fun main() {
+    println(
+        findDuplicates(
+            intArrayOf(3, 1, 3, 1, 5, 6)
+        )
+    )
+    println(
+        findDuplicates(
+            intArrayOf(10, 2, 5, 10, 9, 1, 1, 4, 3, 7)
+        )
+    )
+}
