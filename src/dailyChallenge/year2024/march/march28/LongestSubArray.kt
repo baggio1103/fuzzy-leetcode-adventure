a484571273df35e5345705f65988b5b7ac1d9a54
baggio1103 <valijanov1103@mail.ru>
2024-03-28 12:09:58 +0500

    Solve daily challenge for March 28

---
+++ src/dailyChallenge/year2024/march/march28/LongestSubArray.kt
@@ -0,0 +1,65 @@
+package dailyChallenge.year2024.march.march28
+
+fun maxSubarrayLength(array: IntArray, k: Int): Int {
+    val map = mutableMapOf<Int, Int>()
+    var maxLength = 1
+    var j = 0
+    map[array[0]] = 1
+    for (i in 1 until array.size) {
+        val value = array[i]
+        var count = (map[value] ?: 0) + 1
+        map[value] = count
+        if (count > k) {
+            while (count > k && j <= i) {
+                if (array[j] == value) {
+                    count--
+                }
+                val decr = map[array[j]] ?: 0
+                map[array[j]] = decr - 1
+                j++
+            }
+            map[value] = count
+        }
+        maxLength = maxOf(maxLength, i - j + 1)
+    }
+    return maxLength
+}
+
+fun main() {
+    println(
+        maxSubarrayLength(
+            intArrayOf(1, 2, 3, 1, 2, 3, 1, 2),
+            2
+        )
+    )
+    println(
+        maxSubarrayLength(
+            intArrayOf(1, 2, 1, 2, 1, 2, 1, 2),
+            1
+        )
+    )
+    println(
+        maxSubarrayLength(
+            intArrayOf(5, 5, 5, 5, 5, 5, 5),
+            4
+        )
+    )
+    println(
+        maxSubarrayLength(
+            intArrayOf(2, 2, 2, 3),
+            1
+        )
+    )
+    println(
+        maxSubarrayLength(
+            intArrayOf(1, 1, 1, 1, 2),
+            2
+        )
+    )
+    println(
+        maxSubarrayLength(
+            intArrayOf(1,2,2,1,3),
+            1
+        )
+    )
+}
