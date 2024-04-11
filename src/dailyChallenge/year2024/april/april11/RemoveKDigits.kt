ac074d63578d0f105cb05011b6e828b1b2eec1c6
baggio1103 <valijanov1103@mail.ru>
2024-04-11 23:59:25 +0500

    Add daily challenge for April 11

---
+++ src/dailyChallenge/year2024/april/april11/RemoveKDigits.kt
@@ -0,0 +1,13 @@
+package dailyChallenge.year2024.april.april11
+
+fun removeKDigits(num: String, k: Int): String {
+    return ""
+}
+
+fun main() {
+    println(
+        removeKDigits(
+            "1432219", 3
+        )
+    )
+}
