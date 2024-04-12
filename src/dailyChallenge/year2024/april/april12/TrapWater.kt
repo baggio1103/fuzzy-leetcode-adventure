3493f6013dc2ce8ce50b30b8953bb921bb1a7725
baggio1103 <valijanov1103@mail.ru>
2024-04-13 02:15:45 +0500

    Solve daily challenge for April 12

---
+++ src/dailyChallenge/year2024/april/april12/TrapWater.kt
@@ -0,0 +1,42 @@
+package dailyChallenge.year2024.april.april12
+
+fun trap(height: IntArray): Int {
+    var left = 0
+    var right = height.lastIndex
+    var leftMax = 0
+    var rightMax = 0
+    var result = 0
+    while (left < right) {
+        if (height[left] < height[right]) {
+            if (height[left] >= leftMax) {
+                leftMax = height[left]
+            } else result += leftMax - height[left]
+            left++
+        } else {
+            if (height[right] >= rightMax) {
+                rightMax = height[right]
+            } else result += rightMax - height[right]
+            right--
+        }
+    }
+    return result
+}
+
+fun main() {
+    println(
+        trap(
+            height = intArrayOf(0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1)
+        )
+    )
+    println(
+        trap(
+            height = intArrayOf(4, 2, 0, 3, 2, 5)
+        )
+    )
+    println(
+        trap(
+            height = intArrayOf(4, 2, 0, 3, 2, 5, 7)
+        )
+    )
+
+}
