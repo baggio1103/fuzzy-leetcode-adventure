1fcd8cfc4d0e7e74b9747a6468fee8a9c76ac80a
baggio1103 <valijanov1103@mail.ru>
2024-05-05 22:55:19 +0300

    Solve daily challenge for May 4

--- src/dailyChallenge/year2024/may/may4/RescueBoats.kt
+++ src/dailyChallenge/year2024/may/may4/RescueBoats.kt
@@ -1,39 +1,39 @@
-//package dailyChallenge.year2024.may.may4
-//
-//fun numRescueBoats(people: IntArray, limit: Int): Int {
-//    people.sortDescending()
-//    var left = 0
-//    var right = people.lastIndex
-//    var boats = 0
-//    while (left <= right) {
-//        boats++
-//        if (people[left] + people[right] <= limit) {
-//            left++
-//            right--
-//            continue
-//        }
-//        left++
-//    }
-//    return boats
-//}
-//
-//fun main() {
-//    println(
-//        numRescueBoats(
-//            people = intArrayOf(3, 2, 2, 1),
-//            limit = 3
-//        )
-//    )
-//    println(
-//        numRescueBoats(
-//            people = intArrayOf(3, 5, 3, 4),
-//            limit = 5
-//        )
-//    )
-//    println(
-//        numRescueBoats(
-//            people = intArrayOf(1, 2),
-//            limit = 3
-//        )
-//    )
-//}
+package dailyChallenge.year2024.may.may4
+
+fun numRescueBoats(people: IntArray, limit: Int): Int {
+    people.sortDescending()
+    var left = 0
+    var right = people.lastIndex
+    var boats = 0
+    while (left <= right) {
+        boats++
+        if (people[left] + people[right] <= limit) {
+            left++
+            right--
+            continue
+        }
+        left++
+    }
+    return boats
+}
+
+fun main() {
+    println(
+        numRescueBoats(
+            people = intArrayOf(3, 2, 2, 1),
+            limit = 3
+        )
+    )
+    println(
+        numRescueBoats(
+            people = intArrayOf(3, 5, 3, 4),
+            limit = 5
+        )
+    )
+    println(
+        numRescueBoats(
+            people = intArrayOf(1, 2),
+            limit = 3
+        )
+    )
+}
