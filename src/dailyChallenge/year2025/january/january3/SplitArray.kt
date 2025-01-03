fc6bc0e53a25b108e49b95193907f652b8e76b3f
baggio1103 <valijanov1103@mail.ru>
2025-01-03 23:58:53 +0500

    Solve daily challenge for January 3

---
+++ src/dailyChallenge/year2025/january/january3/SplitArray.kt
@@ -0,0 +1,28 @@
+package dailyChallenge.year2025.january.january3
+
+fun main() {
+    println(
+        waysToSplitArray(nums = intArrayOf(10, 4, -8, 7))
+    )
+    println(
+        waysToSplitArray(nums = intArrayOf(2, 3, 1, 0))
+    )
+    println(
+        waysToSplitArray(nums = intArrayOf(6, -1, 9))
+    )
+}
+
+fun waysToSplitArray(nums: IntArray): Int {
+    val sumArray = IntArray(nums.size)
+    sumArray[0] = nums[0]
+    for (i in 1 until nums.size) {
+        sumArray[i] = nums[i] + sumArray[i - 1]
+    }
+    var count = 0
+    val overallSum = sumArray.last()
+    for (i in 0 until nums.lastIndex) {
+        if (sumArray[i] >= overallSum - sumArray[i]) count++
+    }
+    return count
+}
+
