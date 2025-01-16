df03dac19aea48556c9c496d6797b2a14a4cafaa
baggio1103 <valijanov1103@mail.ru>
2025-01-16 23:12:05 +0500

    Refine solution

--- src/dailyChallenge/year2025/january/january16/Allxors.kt
+++ src/dailyChallenge/year2025/january/january16/Allxors.kt
@@ -1,6 +1,16 @@
 package dailyChallenge.year2025.january.january16
 
 fun main() {
+    val a = 2
+    val b = 3
+    val c = a xor a xor a xor a xor a xor a xor b
+    println(c)
+    // 1 2 3 4 2 1
+    // 2 3 2 8 9 7
+    // --->>> 1 xor (2 3 2 8 9 7) xor 2 xor (2 3 2 8 9 7)
+
+
+
     println(
         xorAllNums(
             arrayOne = intArrayOf(1, 2),
@@ -10,11 +20,18 @@ fun main() {
 }
 
 fun xorAllNums(arrayOne: IntArray, arrayTwo: IntArray): Int {
-    var i = 0
-    arrayOne.forEach { numOne ->
-        arrayTwo.forEach { numTwo ->
-            i = i xor numOne xor numTwo
+    var result = 0
+    val frequencyMap = mutableMapOf<Int, Int>()
+    for (value in arrayOne) {
+        frequencyMap[value] = frequencyMap.getOrDefault(value, 0) + arrayTwo.size
+    }
+    for (value in arrayTwo) {
+        frequencyMap[value] = frequencyMap.getOrDefault(value, 0) + arrayOne.size
+    }
+    frequencyMap.forEach { (key, value) ->
+        if (value % 2 == 1) {
+            result = result xor key
         }
     }
-    return i
+    return result
 }
