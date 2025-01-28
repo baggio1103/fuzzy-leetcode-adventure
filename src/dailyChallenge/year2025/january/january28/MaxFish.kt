d2f49ed745511347ec5e39c14feaaeb76c29fc3e
baggio1103 <valijanov1103@mail.ru>
2025-01-28 15:56:25 +0500

    Refactor solution

--- src/dailyChallenge/year2025/january/january28/MaxFish.kt
+++ src/dailyChallenge/year2025/january/january28/MaxFish.kt
@@ -25,24 +25,20 @@ fun findMaxFish(grid: Array<IntArray>): Int {
     var maxCount = 0
     val rows = grid.size
     val columns = grid[0].size
-    val visited = Array(rows) { BooleanArray(columns) { false } }
-
     fun adjacentEdges(row: Int, column: Int) =
         listOf(Pair(row - 1, column), Pair(row + 1, column), Pair(row, column + 1), Pair(row, column - 1))
 
     fun search(i: Int, j: Int): Int {
         val queue = ArrayDeque<Pair<Int, Int>>().apply { add(Pair(i, j)) }
-        visited[i][j] = true
         var count = 0
         while (queue.isNotEmpty()) {
             val (row, column) = queue.removeFirst()
-            visited[row][column] = true
             val value = grid[row][column]
+            grid[row][column] = 0
             count += value
             adjacentEdges(row, column).filter { (r, c) ->
-                (r in 0 until rows && c in 0 until columns) && !visited[r][c] && grid[r][c] > 0
+                (r in 0 until rows && c in 0 until columns) && grid[r][c] > 0
             }.forEach {
-                visited[it.first][it.second] = true
                 queue.add(it)
             }
         }
@@ -51,7 +47,7 @@ fun findMaxFish(grid: Array<IntArray>): Int {
 
     for ((i, row) in grid.withIndex()) {
         for ((j, cell) in row.withIndex()) {
-            if (cell > 0 && !visited[i][j]) {
+            if (cell > 0) {
                 maxCount = maxOf(maxCount, search(i, j))
             }
         }
