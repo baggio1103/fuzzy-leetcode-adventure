b59ce9df7b271e655091a5351cf8198fd518ef14
baggio1103 <valijanov1103@mail.ru>
2025-02-13 13:14:29 +0500

    Solve daily challenge for February 13

---
+++ src/dailyChallenge/year2025/february/february13/MinOperations.kt
@@ -0,0 +1,26 @@
+package dailyChallenge.year2025.february.february13
+
+import java.util.*
+
+fun main() {
+//    println(
+//        minOperations(intArrayOf(2, 11, 10, 1, 3), 10)
+//    )
+    println(
+        minOperations(intArrayOf(1000000000, 999999999, 1000000000, 999999999, 1000000000, 999999999), 1000000000)
+    )
+}
+
+fun minOperations(array: IntArray, k: Int): Int {
+    var count = 0
+    val queue = PriorityQueue<Long>().apply {
+        array.forEach { add(it.toLong()) }
+    }
+    while (queue.size >= 2 && queue.peek() < k) {
+        val x = queue.poll()
+        val y = queue.poll()
+        queue.offer(2 * minOf(x, y) + maxOf(x, y))
+        count++
+    }
+    return count
+}
