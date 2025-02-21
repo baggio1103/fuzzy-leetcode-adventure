52dca56b1eaeb4625defae29a3f6f91775c29716
baggio1103 <valijanov1103@mail.ru>
2025-02-21 08:55:27 +0500

    Solve daily challenge for February 21

---
+++ src/dailyChallenge/year2025/february/february21/ContaminatedBinaryTree.kt
@@ -0,0 +1,46 @@
+package dailyChallenge.year2025.february.february21
+
+
+fun main() {
+    val node = TreeNode(-1).apply {
+        left = TreeNode(-1).apply {
+            left = TreeNode(-1)
+            right = TreeNode(-1)
+        }
+        right = TreeNode(-1)
+    }
+    println(node)
+    val findElements = FindElements(node)
+    println(findElements)
+
+}
+
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
+
+class FindElements(private val root: TreeNode?) {
+
+    init {
+        root?.`val` = 0
+        fun recover(node: TreeNode?) {
+            if (node == null) return
+            node.left?.`val` = 2 * node.`val` + 1
+            recover(node.left)
+            node.right?.`val` = 2 * node.`val` + 2
+            recover(node.right)
+        }
+        recover(root)
+    }
+
+    fun find(target: Int): Boolean {
+        fun search(node: TreeNode?): Boolean {
+            if (node == null) return false
+            if (node.`val` == target) return true
+            return search(node.left) && search(node.right)
+        }
+        return search(root)
+    }
+
+}
