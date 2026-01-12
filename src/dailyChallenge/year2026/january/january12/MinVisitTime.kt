7f669743d6c2803f198c0d03ce35fe255dd7b240
baggio1103 <valijanov1103@mail.ru>
2026-01-12 13:52:02 +0500

    Solve daily challenge for January 12

---
+++ src/dailyChallenge/year2026/january/january12/MinVisitTime.kt
@@ -0,0 +1,24 @@
+package dailyChallenge.year2026.january.january12
+
+import kotlin.math.abs
+import kotlin.math.absoluteValue
+
+fun main() {
+    println(
+        minTimeToVisitAllPoints(
+            arrayOf(
+                intArrayOf(1, 1), intArrayOf(3, 4), intArrayOf(-1, 0)
+            )
+        )
+    )
+}
+
+fun minTimeToVisitAllPoints(points: Array<IntArray>): Int {
+    var time = 0
+    for (i in 1 until points.size) {
+        val (x1, y1) = points[i - 1]
+        val (x2, y2) = points[i]
+        time += maxOf(abs(x1 - x2), abs(y1 - y2))
+    }
+    return time
+}
